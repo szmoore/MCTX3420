808ad5fb1e9040379ad754fee330b17d431ff3d4
Sam Moore <matches@ucc.asn.au>
2013-10-20 13:02:54 +0800

    Add UserCake
    We might be able to build on this to make a user management system.
    
    UserCake Version: 2.0.2
    http://usercake.com
    
    This commit includes the original UserCake source.
    Many files will need to be removed and/or rewritten.
    
    UserCake uses MySQL for storing user information.
    There is a C API for interfacing with MySQL. Hopefully that will work.

---
+++ testing/MCTXWeb/public_html/users/models/db-settings.php
@@ -0,0 +1,37 @@
+<?php
+/*
+UserCake Version: 2.0.2
+http://usercake.com
+*/
+
+//Database Information
+$db_host = "localhost"; //Host address (most likely localhost)
+$db_name = "users"; //Name of Database
+$db_user = "root"; //Name of database user
+$db_pass = "NOT_THE_PASSWORD"; //Password for database user
+$db_table_prefix = "uc_";
+
+GLOBAL $errors;
+GLOBAL $successes;
+
+$errors = array();
+$successes = array();
+
+/* Create a new mysqli object with database connection parameters */
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+GLOBAL $mysqli;
+
+if(mysqli_connect_errno()) {
+	echo "Connection Failed: " . mysqli_connect_errno();
+	exit();
+}
+
+//Direct to install directory, if it exists
+if(is_dir("install/"))
+{
+	header("Location: install/");
+	die();
+
+}
+
+?>
