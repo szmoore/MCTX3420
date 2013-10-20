bd5fabfeabdd16cf38642c8cfb2232aab495de31
Sam Moore <matches@ucc.asn.au>
2013-10-20 13:02:54 +0800

    Add UserCake
    We might be able to build on this to make a user management system.
    
    UserCake Version: 2.0.2
    http://usercake.com
    
    This commit includes the original UserCake source.
    Many files will need to be removed and/or rewritten.
    
    UserCake uses MySQL for storing user information.
    There is a C API for interfacing with MySQL. Hopefully that will work.

---
+++ testing/MCTXWeb/public_html/users/admin_upload_users.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("models/config.php");
+if (!securePage($_SERVER['PHP_SELF'])){die();}
+
+require_once("models/header.php");
+
+
+if (!empty($_POST))
+{
+  echo "<p> Uploaded! </p>";
+}
+else
+{
+  echo "<p> Please provide a CSV file of usernames and email addresses. </p>
+  <div class=\"title\">Upload</div>
+  <form name='newUser' action='".$_SERVER['PHP_SELF']."' method='post'>
+  <input type=\"file\" name=\"users\"/>
+  <input type=\"submit\" value=\"Upload\"/>
+  </form>";
+}
+  
+?> 
+
